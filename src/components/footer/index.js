e0bd69047ddd31acac6214696ded1b0558c1ada2
Gonzalo Núñez <gonzalonu99@gmail.com>
2023-06-09 16:57:35 -0300

    feat footer

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,33 +1,105 @@
-import React, {useEffect} from 'react'
-import './footer.css'
-import Aos from 'aos'
-import 'aos/dist/aos.css'
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faGithub, faLinkedin, faInstagram} from '@fortawesome/free-brands-svg-icons'
-
-const FooterSection = () => {
-  useEffect(() => {
-    Aos.init({duration:1000})
-  }, []);
+import {
+  Box,
+  Container,
+  Link,
+  List,
+  ListItem,
+  ListItemIcon,
+  Typography,
+} from "@mui/material";
+import React from "react";
+import Logo from "../../images/gnLogo.png";
+import "./footer.css";
+import WhatsAppIcon from "@mui/icons-material/WhatsApp";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import InstagramIcon from "@mui/icons-material/Instagram";
+const Footer = () => {
   return (
-    <div id='contact' className='footer-section' >
-      <div className="animation" data-aos="fade-right">
-        <h3 className="footer-title"><b className='footer-b'>Gonzalo Núñez</b></h3>
-                <p className='footer-text'>Feel free to contact me</p>
-                <div className="buttons" >
-                    <button className='social-media-button'>
-                    <a className='social-media-icon' href="https://github.com/Gonzalonu99"><FontAwesomeIcon icon={faGithub} /></a>
-                </button>
-                <button className='social-media-button'>
-                    <a className='social-media-icon' href="https://www.linkedin.com/in/gonzalo-n%C3%BA%C3%B1ez-5a506a221/"><FontAwesomeIcon icon={faLinkedin}/></a>
-                </button>
-                <button className='social-media-button'>
-                    <a className='social-media-icon' href="https://www.instagram.com/gonzalo.nunez99/"><FontAwesomeIcon icon={faInstagram}/></a>
-                </button>
-                </div>
-              </div>
+    <div id="footer">
+      <Container className="footer-left">
+        <Box className="logo-footer-container">
+          <img
+            src={Logo}
+            alt="Logo-footer"
+            style={{ width: "12rem" }}
+            className="footer-logo"
+          />
+          <Typography
+            className="footer-logo-text"
+            variant="h5"
+            sx={{ fontFamily: "monospace", fontWeight: "bold", color: "#fff" }}
+          >
+            NÚÑEZ
+          </Typography>
+        </Box>
+        <Box
+          className="social-icons-container"
+          sx={{ display: "flex", justifyContent: "flex-start" }}
+        >
+          <Link
+            className="social-icons-link"
+            href="https://api.whatsapp.com/send?phone=541139371486"
+          >
+            <WhatsAppIcon className="social-icon" />
+          </Link>
+          <Link
+            className="social-icons-link"
+            href="https://www.linkedin.com/in/gonzalo-nunez-web-developer/"
+          >
+            <LinkedInIcon className="social-icon" />
+          </Link>
+          <Link
+            className="social-icons-link"
+            href="https://github.com/Gonzalonu99"
+          >
+            <GitHubIcon className="social-icon" />
+          </Link>
+          <Link
+            className="social-icons-link"
+            href="https://www.instagram.com/gonzalo.nunez99/"
+          >
+            <InstagramIcon className="social-icon" />
+          </Link>
+        </Box>
+      </Container>
+      <Container className="footer-list-con">
+        <List>
+          <ListItem>
+            <ListItemIcon>
+              <span className="sk-list-icon">▶</span>
+            </ListItemIcon>
+            <Typography variant="body1" className="footer-item-text">
+              B.A., Argentina
+            </Typography>
+          </ListItem>
+          <ListItem>
+            <ListItemIcon>
+              <span className="sk-list-icon">▶</span>
+            </ListItemIcon>
+            <Typography variant="body1" className="footer-item-text">
+              +541139371486
+            </Typography>
+          </ListItem>
+          <ListItem>
+            <ListItemIcon>
+              <span className="sk-list-icon">▶</span>
+            </ListItemIcon>
+            <Typography variant="body1" className="footer-item-text">
+              <Link href="mailto:gonzalonu99@gmail.com" color="inherit">
+                gonzalonu99@gmail.com
+              </Link>
+            </Typography>
+          </ListItem>
+        </List>
+      </Container>
+      <div className="footer-rights">
+        <Typography className="ft-rights-text">
+          &copy; Designed and built by Gonzalo Núñez
+        </Typography>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default FooterSection
+export default Footer;
