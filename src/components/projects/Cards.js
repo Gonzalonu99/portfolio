9a0a05e0b1b203471bfe4df4a88d3f76a9c4b6f9
Gonzalo Núñez <gonzalonu99@gmail.com>
2023-06-09 16:56:52 -0300

    feat project section

--- src/components/projects/Cards.js
+++ src/components/projects/Cards.js
@@ -1,109 +1,61 @@
-import Carousel from "react-multi-carousel";
-import 'react-multi-carousel/lib/styles.css'
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faArrowUpRightFromSquare} from '@fortawesome/free-solid-svg-icons'
-import CalculatorPhoto from '../../images/calculator.png'
-import ClimaPhoto from '../../images/clima.png'
-import WinesPhoto from '../../images/wines.png'
-import BackendPhoto from '../../images/proyecto-backend.png'
-import AutoshopPhoto from '../../images/autoshop.png'
-import { useEffect } from "react";
-import Aos from 'aos'
-import 'aos/dist/aos.css'
+import React from "react";
+import ClimaPhoto from "../../images/clima.png";
+import BackendPhoto from "../../images/proyecto-backend.png";
+import AutoshopPhoto from "../../images/autoshop.png";
 
-const responsive = {
-    desktop: {
-        breakpoint: {max: 3000, min: 1024},
-        items: 3,
+const Projects = () => {
+  const projectArray = [
+    {
+      id: "Autoshop",
+      img: AutoshopPhoto,
+      title: "AutoShop-365",
+      link: "http://a365.com.ar/",
     },
-    tablet: {
-        breakpoint: {max: 1024, min: 464},
-        items:3,
-        partialVisibilityGutter: -100
+    {
+      id: "projectsWeather-app",
+      img: ClimaPhoto,
+      title: "Weather App",
+      link: "https://nunez-weatherapp.web.app",
     },
-    mobile: {
-        breakpoint: {max:464, min:0},
-        items: 1,
-        partialVisibilityGutter:110
-    }
-};
-const Gallery = ()=>{
-    useEffect(() => {
-        Aos.init({duration:1000})
-      }, []);
-    const cartas = [
-        {
-            id: "projectsCalculator",
-            img: CalculatorPhoto,
-            title: "Calculator App",
-            repoLink: "https://github.com/Gonzalonu99/calculator-app",
-            demoLink: "https://nunez-calculator.web.app"
-        },
-        {
-            id: "projectsWeather-app",
-            img: ClimaPhoto,
-            title: "Weather App",
-            repoLink: "https://github.com/Gonzalonu99/weather-app",
-            demoLink: "https://nunez-weatherapp.web.app"
-        },
-        {
-            id: "projectsNumen-wines",
-            img: WinesPhoto,
-            title: "Numen Wines",
-            repoLink: "https://github.com/Gonzalonu99/numen-wines",
-            demoLink: "https://numenwines.web.app/"
-        },
-        {
-            id: "projectsMongoDB",
-            img: BackendPhoto,
-            title: "Cymbals CRUD",
-            repoLink: "https://github.com/Gonzalonu99/backend-express-mongoDB",
-            demoLink: ""
-        },
-        {
-            id: "Autoshop",
-            img: AutoshopPhoto,
-            title: "AutoShop web",
-            repoLink: "https://github.com/Gonzalonu99/autoshop-web",
-            demoLink:"http://a365.com.ar/"
-        }
-    ]
-    return(
-       <Carousel
-            responsive={responsive}
-            partialVisbile={true}
-            ssr={true}
-            infinite={true}
-            autoPlay={false}
-            autoPlaySpeed={1600}
-            customTransition= "all .5"
-            transitionDuration={500}
-            containerClass="carousel-container"
-            removeArrowOnDeviceType={["tablet","mobile"]}
-            itemClass="carousel-item-padding-40-px"
+    {
+      id: "projectsMongoDB",
+      img: BackendPhoto,
+      title: "Cymbals CRUD",
+      link: "https://github.com/Gonzalonu99/backend-express-mongoDB",
+    },
+  ];
+  const handleProjectClick = (link) => {
+    window.open(link, "_blank");
+  };
+
+  return (
+    <div className="pr-flex">
+      {projectArray.map((project, index) => (
+        <div
+          key={index}
+          className='project-box'
+          style={{
+            position: "relative",
+            width: "350px",
+            height: "350px",
+            margin: "10px",
+            overflow: "hidden",
+          }}
+          onClick={() => handleProjectClick(project.link)}
         >
-            {cartas.map((carta)=>(
-                <div className="card-container" key={carta.id} data-aos="fade-up">
-                    <div className="img-card-container">
-                        <img src={carta.img} alt="img" className="card-img"/>
-                    </div>
-                    <h5 className="card-subtitle">{carta.title}</h5>
-                    <button className="repo-link-button">
-                        <a href={carta.repoLink}>
-                        <FontAwesomeIcon icon={faArrowUpRightFromSquare}/>  View Repository
-                        </a>
-                    </button>
-                    <button className="demo-link-button">
-                        <a href={carta.demoLink}>
-                        <FontAwesomeIcon icon={faArrowUpRightFromSquare}/>  View Project
-                        </a>
-                    </button>
-                </div>
-            ))}
-        </Carousel> 
-    )
+          <div
+            className="project-image"
+            style={{
+              backgroundImage: `url(${project.img})`,
+            }}
+          />
+          <div className="project-overlay">
+            <div className="project-title">{project.title}</div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
 
-}
-
-
-export default Gallery
+export default Projects;
--- src/components/projects/index.js
+++ src/components/projects/index.js
@@ -1,14 +1,29 @@
-import React from 'react'
-import Gallery from './Cards'
-import './projects.css'
+import React from "react";
+import "./projects.css";
+import { Box, Typography } from "@mui/material";
+import Projects from "./Cards";
 
 const ProjectSection = () => {
   return (
-    <div className='projects-section' id='projects'>
-        <h1 className='skill-title'>My recents  <span className='span-title'>Works</span></h1>
-        <Gallery/>
+    <div id="projects">
+      <Box className='project-st-container'>
+        <Typography
+          className="project-st"
+          variant="h2"
+          sx={{
+            fontFamily: "Playfair Display, serif",
+            fontWeight: "bold",
+            width: "auto",
+          }}
+        >
+          Some Projects
+        </Typography>
+      </Box>
+      <div>
+        <Projects/>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProjectSection
+export default ProjectSection;
