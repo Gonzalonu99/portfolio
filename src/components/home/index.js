f111bcdf4cef3650484f8c7f65f345d582dd6bd0
Gonzalo Núñez <gonzalonu99@gmail.com>
2023-06-09 16:55:51 -0300

    feat home

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,26 +1,104 @@
-import React, {useEffect} from 'react'
-import Typewriter from './Typewriter'
-import HomePhoto from '../../images/home-man-photo_adobe_express.png'
-import './home.css'
-import Aos from 'aos'
-import 'aos/dist/aos.css'
+import React from "react";
+import { Box, Button, Divider, Typography } from "@mui/material";
+import "../home/home.css";
+import LogoGN from "../../images/gnSombraLogo.png";
+import fotoGN from "../../images/fotoSinFondo.png";
+import pdfCv from '../../assets/NunezCVEspañol.pdf';
 
 const HomeSection = () => {
-  useEffect(() => {
-    Aos.init({duration:1000})
-  }, []);
+  const descargarArchivoHome = () => {
+    const link = document.createElement('a');
+    link.href = pdfCv;
+    link.download = 'NunezCVEspañol.pdf';
+    link.click();
+  };
   return (
-    <>
-        <div className='home-container' data-aos="fade-up">
-            <div className='text-container'>
-                <h3>Hi there!</h3>
-                <h2>I'm <h2 className='mi-nombre'>Gonzalo Núñez</h2></h2>
-                <h1 className='type-text'><Typewriter/></h1>
-            </div>
-            <div className='image-container'><img src={HomePhoto} className='home-image'/></div>
+    <div className="home" id="home-section">
+      <Box className="home-fs-subtitle">
+        <Divider
+          sx={{
+            backgroundColor: "#046380",
+            flexGrow: 1,
+            height: "2px",
+            width: "7rem",
+          }}
+          className="home-line-subtitle"
+        />
+        <Typography
+          className="home-subtitle"
+          sx={{ paddingLeft: ".5rem", fontSize: "20px", fontWeight: "bold", fontFamily: "Playfair Display, serif" }}
+        >
+          Fullstack Developer
+        </Typography>
+      </Box>
+      <Box className="home-h1-box">
+        <Typography
+          className="home-title"
+          variant="h2"
+          sx={{ fontFamily: "Playfair Display, serif", fontWeight: "bold" }}
+        >
+          Hello there !
+        </Typography>{" "}
+        <br />
+        <Typography
+          className="home-title"
+          variant="h2"
+          sx={{ fontFamily: "Playfair Display, serif", fontWeight: "bold" }}
+        >
+          I'm <span className="home-span">G</span>onzalo
+        </Typography>{" "}
+        <br />
+        <Typography
+          className="home-title"
+          variant="h2"
+          sx={{ fontFamily: "Playfair Display, serif", fontWeight: "bold" }}
+        >
+          <span className="home-span">N</span>úñez.
+        </Typography>
+      </Box>
+      <Box className="home-text-btn-box">
+        <Typography
+          className="home-text"
+          variant="h6"
+          sx={{ fontFamily: "Playfair Display, serif" }}
+        >
+          I'm a Fullstack Developer from Buenos Aires, Argentina.
+          <br /> I'm a React fan and I enjoy building effective and functional
+          web applications. <br />
+          I'm here to help you. Let's work together to bring your ideas to life!
+        </Typography>
+        <Button
+          className="home-dw-btn"
+          sx={{
+            backgroundColor: "#046380",
+            color: "#fff",
+            display: {
+              xs: "flex",
+              sm: "none",
+              md: "none",
+              lg: "none",
+              xl: "none",
+            },
+            position: "relative",
+            marginTop: "20px",
+            marginLeft: "auto",
+            marginRight: "auto",
+          }}
+          onClick={descargarArchivoHome}
+        >
+          Download CV
+        </Button>
+      </Box>
+      <div className="img-logo-container">
+        <div className="logo-container">
+          <img src={LogoGN} alt="Logo-GN" className="home-logo-grande" />
         </div>
-    </>
-  )
-}
+        <div className="foto-container">
+          <img src={fotoGN} alt="Foto-GN" className="home-foto-grande" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default HomeSection
+export default HomeSection;
